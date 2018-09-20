5c1aacef159034f3f40d348301871d19387c382b
jerry kim <jerry.kim@nio.io>
2018-09-20 14:09:15 -0700

    token parser works

--- python/regex/token_parser.py
+++ python/regex/token_parser.py
@@ -6,33 +6,47 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-OPEN=re.compile(r'(\d+)[')
+OPEN=re.compile(r'(\d+)\[')
 
-def find_matched_brack(data):
+def find_matched_bracket(data):
     stack=[]
-    for achar in data:
-        if achar == '[':
+    for i, _ in enumerate(data):
+        if data[i] == '[':
+            stack.append('[')
+        elif data[i] == ']':
+            if len(stack) > 0:
+                stack.pop()
 
-    
+        if len(stack) == 0:
+            return i
 
-def expansion(data, num=0):
+    return -1
+
+def expansion(data):
     m = re.search(OPEN, data)
     if m is None:
-        if num > 0:
-            index=data.index(CLOSE)
+        return data
 
+    prev = data[:m.start()]
+    rest = data[m.end()-1:]
 
+    matched = find_matched_bracket(rest)
+    if matched < 0:
+        return prev + m.group(0) + rest[1:]
 
+    repeat = int(m.group(1)) * expansion(rest[1:matched])
+
+    last = expansion(rest[matched+1:])
+
+    return prev + repeat + last
 
 
 
 def main():
-    #data=['abc', '10[abc]','10[ab3[c]d]', 'abc10[abc3[de]fg]hij', 'abc2[d]fgh3[i]']
-    data=['abc2[d]fgh3[i]']
+    data=['abc', '10[abc]','10[ab3[c]d]', 'abc10[abc3[de]fg]hij', 'abc2[d]fgh3[i]', 'abc5[i10[aa]5[z]]fff3[ab2[yx3[z]i]]']
     for item in data:
         print(item, expansion(item))
 
-    print stack
 
 if __name__ == "__main__":
     main()
