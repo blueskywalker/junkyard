c9ab10bf420b98323c772013ea74effc8a99a272
jerry kim <kyungseop.kim@gmail.com>
2018-09-22 23:09:00 -0700

    add recursive

--- python/regex/recursive_parse.py
+++ python/regex/recursive_parse.py
@@ -1,28 +1,64 @@
 
 import re
 
-pattern = r'(\d+)\[(.*)\]'
 
-tokenizer=re.compile(pattern)
+OPEN=r'(\d+)\['
 
-def expansion(data):
+def matched_length(t):
+    return t[1] - t[0]
+    
+def find_match(data):
+    m = re.match(OPEN, data)    
+    if m is None:
+        raise ValueError('wrong string {data}'.format(**locals()))
+    
+    last=m.end()
+    rest = data[last:]
+    
+    while last< len(data):
+        close = rest.index(']')
+        if close == -1:
+            raise SyntaxError()
 
-    def expand(times, data):
-        return expansion(data) * int(times)
+        m = re.search(OPEN, rest)
+        if m is None:
+            return last + close
+        
+        start = m.start()
+        if close < start:
+            return last + close
+        
+        submatch = find_match(rest[m.start():])
+        last = last+ start+submatch+1
+        rest = data[last:]
+        
+    
+    raise ValueError(data)
 
-    matched = tokenizer.search(data)
-    if matched is None:
+def expansion(data):    
+
+    
+    m = re.search(OPEN, data)
+    
+    if m is None:
         return data
+    
+    prev = data[:m.start()]    
+    rest = data[m.start():]
+    
+    matched = find_match(rest)
+    expr = rest[matched_length(m.span()):matched]
+    repeated = int(m.group(1)) * expansion(expr)
+    last = expansion(rest[matched+1:])
+    return prev + repeated + last
 
+def main():
+    
+    data = ['abc', '10[abc]','10[ab3[c]d]', 'abc10[abc3[de]fg]hij', 'abc2[d]fgh3[i]', 'abc5[i2[aa]5[z]]fff3[ab2[yx3[z]i]]' ]
+    #data = ['abc5[i2[aa]5[z]]fff3[ab2[yx3[z]i]]']
+    for item in data:
+        print(item, expansion(item))
 
-    pre = data[:matched.start()]
-    expanded = expand(matched.group(1), matched.group(2)) 
-    post = expansion(data[matched.end()+1:])
-
-    print("{pre}:{expanded}:{post}".format(**locals()))
-    return pre + expanded + post
-
-#data=['abc', '10[abc]','10[ab3[c]d]', 'abc10[abc3[de]fg]hij', 'abc2[d]fgh3[i]']
-data=['abc2[d]fgh3[i]']
-for item in data:
-    print(item, expansion(item))
+        
+if __name__ == "__main__":
+    main()
