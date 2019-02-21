f16d363ce8a4ea070ca2589aa1d0e4f7924d324f
jerry kim <jerry.kim@nio.io>
2019-02-21 14:57:05 -0800

    add more type hints

--- python/typing/simple_priority_queue.py
+++ python/typing/simple_priority_queue.py
@@ -1,12 +1,12 @@
-from typing import Any
+from typing import Any, List
 
 
 class SimplePriorityQ(object):
 
     def __init__(self, capacity: int, reverse: bool = False):
-        self.capacity = capacity
-        self.heap=[]
-        self.reverse = reverse
+        self.capacity: int = capacity
+        self.heap: List[Any] =[]
+        self.reverse: bool = reverse
 
     def _add(self, item: Any) -> None:
         self.heap.append(item)
@@ -22,9 +22,11 @@ class SimplePriorityQ(object):
             if self.reverse:
                 if self.heap[-1].__lt__(item):
                     return self._replace(item)
+                return item
             else:
                 if item.__lt__(self.heap[-1]):
                     return self._replace(item)
+                return item
         else:
             return self._add(item)
 
@@ -33,11 +35,11 @@ class SimplePriorityQ(object):
 
 
 def main():
-    queue = SimplePriorityQ(5)
+    queue = SimplePriorityQ(5, reverse=True)
     data = [1, 11, 5, 43, 23, 22, 15, 7, 13, 33]
 
     for item in data:
-        queue.add(item)
+        print(queue.add(item))
 
     print(queue.get_items())
 
