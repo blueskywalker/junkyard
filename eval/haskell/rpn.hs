3c1b6c9582c1d950166c3361cda0e07947bab99e
jerry kim <jerrykskim@gmail.com>
2019-03-24 18:21:32 -0700

    eval fix function name

--- eval/haskell/rpn.hs
+++ eval/haskell/rpn.hs
@@ -1,8 +1,8 @@
 
 import Data.List
 
-sovleRPN :: String -> Float
-sovleRPN = head . foldl foldingFunction [] . words
+solveRPN :: String -> Float
+solveRPN = head . foldl foldingFunction [] . words
          where  foldingFunction (x:y:ys) "*" = (x * y):ys
                 foldingFunction (x:y:ys) "+" = (x + y):ys
                 foldingFunction (x:y:ys) "-" = (y - x):ys
